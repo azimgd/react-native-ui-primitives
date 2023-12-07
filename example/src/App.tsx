179c61b1c944fb6d5005dcd22ec5c34dafb1680d
azim <me@azimgd.com>
2023-12-07 14:49:35 +0500

    chore: regroup example components on home page

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -18,7 +18,7 @@ import {
   Attachment,
   Onetime,
 } from 'react-native-default-ui';
-import { TamaguiProvider, YStack, ScrollView, XStack } from 'tamagui';
+import { TamaguiProvider, YStack, ScrollView, XStack, H4 } from 'tamagui';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 const Noop = () => null;
@@ -39,19 +39,8 @@ export default function App() {
         <SafeAreaView style={styles.safearea}>
           <ScrollView style={styles.container}>
             <YStack space="$4" padding="$4">
-              <Button paint="secondary" onPress={toggleCalendar}>
-                Open Calendar
-              </Button>
-            </YStack>
+              <H4>Buttons</H4>
 
-            <YStack space="$4" padding="$4">
-              <XStack space={4}>
-                <Attachment source={{ uri: '' }} onActionPress={noopHandler} />
-                <Attachment source={{ uri: '' }} onActionPress={noopHandler} />
-              </XStack>
-            </YStack>
-
-            <YStack space="$4" padding="$4">
               <Button paint="primary">Primary</Button>
               <Button paint="secondary">Secondary</Button>
               <Button disabled>Disabled</Button>
@@ -59,6 +48,8 @@ export default function App() {
             </YStack>
 
             <YStack space="$4" padding="$4">
+              <H4>Inputs</H4>
+
               <Input
                 placeholder="Default"
                 label="Full Name"
@@ -67,6 +58,8 @@ export default function App() {
             </YStack>
 
             <YStack space="$4" padding="$4">
+              <H4>Grouped Buttons</H4>
+
               <GroupedContainer>
                 <GroupedButton>John Smith</GroupedButton>
               </GroupedContainer>
@@ -78,6 +71,8 @@ export default function App() {
             </YStack>
 
             <YStack space="$4" padding="$4">
+              <H4>Grouped Inputs</H4>
+
               <GroupedContainer>
                 <GroupedInput label="Firstname" placeholder="Default" />
                 <GroupedInput label="Lastname" placeholder="Default" />
@@ -85,6 +80,8 @@ export default function App() {
             </YStack>
 
             <YStack space="$4" padding="$4">
+              <H4>Grouped Switchs</H4>
+
               <GroupedContainer>
                 <GroupedSwitch checked>Checked</GroupedSwitch>
                 <GroupedSwitch checked={false}>Unchecked</GroupedSwitch>
@@ -92,19 +89,44 @@ export default function App() {
             </YStack>
 
             <YStack space="$4" padding="$4">
+              <H4>Headers</H4>
+
               <Header title="Home Page" iconLeft={<Noop />} />
             </YStack>
 
             <YStack space="$4" padding="$4">
+              <H4>Footers</H4>
+
               <Footer title="Home Page" iconLeft={<Noop />} />
             </YStack>
 
             <YStack space="$4" padding="$4">
+              <H4>Contacts</H4>
+
               <Contact iconLeft={<Noop />} />
             </YStack>
 
             <YStack space="$4" padding="$4">
+              <H4>One time password</H4>
+
               <Onetime numberOfDigits={6} />
+            </YStack>
+
+            <YStack space="$4" padding="$4">
+              <H4>Calendar</H4>
+
+              <Button paint="secondary" onPress={toggleCalendar}>
+                Open Calendar
+              </Button>
+            </YStack>
+
+            <YStack space="$4" padding="$4">
+              <H4>Attachments</H4>
+
+              <XStack space={4}>
+                <Attachment source={{ uri: '' }} onActionPress={noopHandler} />
+                <Attachment source={{ uri: '' }} onActionPress={noopHandler} />
+              </XStack>
             </YStack>
           </ScrollView>
         </SafeAreaView>
