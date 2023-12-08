8a1043e60c201d07a6da4bed93bc8e29dd8c2b25
azim <me@azimgd.com>
2023-12-08 19:26:55 +0500

    chore: password field visibility toggle

--- src/components/Password/index.tsx
+++ src/components/Password/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Pressable } from 'react-native';
 import { Input } from '../Input';
 import { Meter } from './Meter';
 import { passwordStrength, type Result } from 'check-password-strength';
@@ -7,8 +8,9 @@ export const Password = Input.styleable((props, ref) => {
   const [secure, setSecure] = React.useState<boolean>(true);
   const [meter, setMeter] = React.useState<Result<any>>();
 
-  setSecure;
-
+  /**
+   * Password strength meter
+   */
   const handleChangeText = React.useCallback(
     (text: string) => {
       setMeter(passwordStrength(text));
@@ -22,6 +24,18 @@ export const Password = Input.styleable((props, ref) => {
     [props.onChangeText]
   );
 
+  /**
+   * Toggle secure state to show / hide password
+   */
+  const renderIconRight = React.useMemo(
+    () => (
+      <Pressable onPress={() => setSecure((state) => !state)}>
+        {props.iconRight}
+      </Pressable>
+    ),
+    [props.iconRight]
+  );
+
   return (
     <Input
       ref={ref}
@@ -29,6 +43,7 @@ export const Password = Input.styleable((props, ref) => {
       onChangeText={handleChangeText}
       secureTextEntry={secure}
       supplement={<Meter meter={meter} />}
+      iconRight={renderIconRight}
     />
   );
 });
