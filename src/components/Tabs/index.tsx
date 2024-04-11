727527cd04279131dd0a42498b4e56c78355f0f1
azim <me@azimgd.com>
2024-04-11 23:45:08 +0500

    chore: implement android version of tabs component

--- src/components/Tabs/index.tsx
+++ src/components/Tabs/index.tsx
@@ -10,27 +10,62 @@ import type { PressableProps } from 'react-native';
  */
 const TABS_LABEL_FONT_SIZE = Platform.select({
   ios: 13,
-  android: 13,
+  android: 14,
   default: 13,
 });
-const TABS_LABEL_FONT_WEIGHT = Platform.select<'400'>({
+const TABS_BACKGROUND_COLOR = Platform.select({
+  ios: colors.COLOR_DISABLED,
+  android: colors.COLOR_TRANSPARENT,
+  default: colors.COLOR_DISABLED,
+});
+const TABS_BORDER_BOTTOM_COLOR = Platform.select({
+  ios: colors.COLOR_TRANSPARENT,
+  android: colors.COLOR_BORDER,
+  default: colors.COLOR_TRANSPARENT,
+});
+const TABS_BORDER_BOTTOM_WIDTH = Platform.select({
+  ios: 0,
+  android: 1,
+  default: 0,
+});
+const TABS_LABEL_FONT_WEIGHT = Platform.select<'400' | '500'>({
   ios: '400',
-  android: '400',
+  android: '500',
   default: '400',
 });
 const TABS_ITEM_HEIGHT = Platform.select({
   ios: 24,
-  android: 24,
+  android: 48,
   default: 24,
+});
+const TABS_ITEM_BACKGROUND_COLOR = Platform.select({
+  ios: colors.COLOR_WHITE,
+  android: colors.COLOR_TRANSPARENT,
+  default: colors.COLOR_WHITE,
+});
+const TABS_ITEM_PADDING_VERTICAL = Platform.select({
+  ios: 0,
+  android: 14,
+  default: 0,
+});
+const TABS_ITEM_ACTIVE_BORDER_COLOR = Platform.select({
+  ios: colors.COLOR_TRANSPARENT,
+  android: colors.COLOR_PRIMARY,
+  default: colors.COLOR_TRANSPARENT,
+});
+const TABS_ITEM_ACTIVE_BORDER_WIDTH = Platform.select({
+  ios: 0,
+  android: 2,
+  default: 0,
 });
 const TABS_PADDING = Platform.select({
   ios: 2,
-  android: 2,
+  android: 0,
   default: 2,
 });
 const TABS_BORDER_RADIUS = Platform.select({
   ios: 8,
-  android: 8,
+  android: 0,
   default: 8,
 });
 
@@ -49,9 +84,11 @@ export type TabsProps = GetProps<typeof View> & CustomTabsProps;
 
 export const Container = styled(XStack, {
   flex: 1,
-  backgroundColor: colors.COLOR_DISABLED,
+  backgroundColor: TABS_BACKGROUND_COLOR,
   borderRadius: TABS_BORDER_RADIUS,
   padding: TABS_PADDING,
+  borderBottomColor: TABS_BORDER_BOTTOM_COLOR,
+  borderBottomWidth: TABS_BORDER_BOTTOM_WIDTH,
 });
 
 export const Item = styled(View, {
@@ -60,11 +97,14 @@ export const Item = styled(View, {
   alignItems: 'center',
   borderRadius: TABS_BORDER_RADIUS,
   height: TABS_ITEM_HEIGHT,
+  paddingVertical: TABS_ITEM_PADDING_VERTICAL,
 
   variants: {
     active: {
       true: {
-        backgroundColor: colors.COLOR_WHITE,
+        backgroundColor: TABS_ITEM_BACKGROUND_COLOR,
+        borderBottomColor: TABS_ITEM_ACTIVE_BORDER_COLOR,
+        borderBottomWidth: TABS_ITEM_ACTIVE_BORDER_WIDTH,
       },
     },
   },
